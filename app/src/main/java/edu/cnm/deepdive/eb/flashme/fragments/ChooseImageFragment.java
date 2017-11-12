79cf6ab43484bdf6f3be4c242cb2c9917987de7b
ejerson <ejersonb@gmail.com>
2017-11-11 17:25:50 -0700

    TextView for my SpannableStrings are dynamically created.

--- app/src/main/java/edu/cnm/deepdive/eb/flashme/fragments/ChooseImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/eb/flashme/fragments/ChooseImageFragment.java
@@ -1,6 +1,5 @@
 package edu.cnm.deepdive.eb.flashme.fragments;
 
-import android.app.AlertDialog.Builder;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -13,24 +12,24 @@ import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import edu.cnm.deepdive.eb.flashme.R;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChooseImageFragment extends Fragment {
 
   String pig = "Imagine I love candy!";
   String[] arr = pig.split(" ");
-  private SpannableString ss;
+  private SpannableString ssOne, ssTwo, ssThree, ssFour;
+  TextView ssTextView;
+
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    Builder builder = new Builder(getActivity());
-
-    LayoutInflater inflater = getActivity().getLayoutInflater();
-
-    View inflatedView = inflater.inflate(R.layout.image_fragment_view, null);
 
   }
 
@@ -40,23 +39,22 @@ public class ChooseImageFragment extends Fragment {
       @Nullable Bundle savedInstanceState) {
     View v = inflater.inflate(R.layout.image_fragment_view, container, false);
 
-    // TODO turn text into array so I can refer to each index
-
-
-
-    ss = new SpannableString(arr[1]);
     ClickableSpan clickableSpan = new ClickableSpan() {
       @Override
       public void onClick(View textView) {
 
+        Toast.makeText(getActivity(), ssOne, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getActivity(), ssTwo, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getActivity(), ssThree, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getActivity(), ssFour, Toast.LENGTH_SHORT).show();
 
 //        ChooseImageFragment fragment = new ChooseImageFragment();
 //        Bundle args = new Bundle();
-//        args.putInt(DeckMemberFragment.DECK_ID,
-//            getActivity().getIntent().getIntExtra(DeckMemberFragment.DECK_ID, 0));
+////        args.putInt(DeckMemberFragment.DECK_ID,
+////            getActivity().getIntent().getIntExtra(DeckMemberFragment.DECK_ID, 0));
 //        fragment.setArguments(args); // bundle
 //        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
-//
+
 
       }
 
@@ -69,13 +67,36 @@ public class ChooseImageFragment extends Fragment {
 
 
 
-    ss.setSpan(clickableSpan, 0, ss.length(), Spanned.SPAN_COMPOSING);
-//    ss.setSpan(clickableSpan, 1, ss.length() - 1, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
-//
-    TextView textView = v.findViewById(R.id.hello);
-    textView.setText(ss);
-    textView.setMovementMethod(LinkMovementMethod.getInstance());
-    textView.setHighlightColor(Color.TRANSPARENT);
+List<SpannableString> ss = new ArrayList<>();
+
+      ss.add(ssOne = new SpannableString(arr[0]));
+      ss.add(ssTwo = new SpannableString(arr[1]));
+      ss.add(ssThree = new SpannableString(arr[2]));
+      ss.add(ssFour = new SpannableString(arr[3]));
+
+      for (int i = 0; i < ss.size(); i ++ ) {
+       ss.get(i).setSpan(clickableSpan, 0, ss.get(i).length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+      }
+
+    LinearLayout rl = v.findViewById(R.id.image_layout);
+
+    TextView[] ssTv = new TextView[arr.length];
+
+    for(int i=0; i< arr.length; i++)
+    {
+      ssTv[i] = new TextView(getActivity());
+      // TODO assign each TextView with its own SpannableString
+      ssTv[i].setText(ss.get(1));
+      ssTv[i].setTextSize(20);
+      ssTv[i].setMovementMethod(LinkMovementMethod.getInstance());
+      ssTv[i].setHighlightColor(Color.TRANSPARENT);
+
+      ssTv[i].setPadding(50, 50, 0, 0);
+      ssTv[i].setTextColor(Color.parseColor("#000000"));
+      rl.addView(ssTv[i]);
+    }
+
+
     // TODO the back of a card turns into something clickable which will search my api for images/ 1 max for now
     // TODO make sure the chosen keyword gets loaded along with a new fragment
 
